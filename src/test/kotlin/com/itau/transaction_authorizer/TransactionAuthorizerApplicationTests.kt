20ef63c3c90abfb43d5fb8d5304a9f0a9da6471d
Renato-RJ <renato_santosgs@hotmail.com>
2026-06-18 15:33:02 -0300

    IT-001 - Criando estrutura inicial projeto.

---
+++ src/test/kotlin/com/itau/transaction_authorizer/TransactionAuthorizerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.itau.transaction_authorizer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TransactionAuthorizerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
