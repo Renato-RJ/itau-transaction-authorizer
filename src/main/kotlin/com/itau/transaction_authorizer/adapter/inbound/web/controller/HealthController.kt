37f6241763928c0d283e8b0cb919befafaf4f410
Renato-RJ <renato_santosgs@hotmail.com>
2026-06-24 15:13:30 -0300

    IT-002 - Remoção imports não utilizados

--- src/main/kotlin/com/itau/transaction_authorizer/adapter/inbound/web/controller/HealthController.kt
+++ src/main/kotlin/com/itau/transaction_authorizer/adapter/inbound/web/controller/HealthController.kt
@@ -1,8 +1,5 @@
 package com.itau.transaction_authorizer.adapter.inbound.web.controller
 
-import org.springframework.http.HttpStatus
-import org.springframework.http.HttpStatus.OK
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
